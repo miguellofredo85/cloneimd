61d203816c80959a2e2c30dc2a5dde23e8953be0
Miguel Lofredo <miguellofredo@gmail.com>
2023-06-14 02:08:39 -0300

    fix name

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,7 +9,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'IMDClone'
+  title: 'IMDbClone'
 }
 
 export default function RootLayout({ children }) {
