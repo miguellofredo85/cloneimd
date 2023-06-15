dbb3c4f4f94581644022c9d2d7b4d4532c911acc
Miguel Lofredo <miguellofredo@gmail.com>
2023-06-14 21:09:44 -0300

    add movieId route and displaying info

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,7 +1,6 @@
 import Card from "@/components/Card";
 
 export default function Result({results}) {
-  console.log(results);
   return (
     <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       { results.map((result) =>(
